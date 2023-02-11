4459faf954a27644f3bb1f79ed38a726286bb8ec
Islam Monijd <islam.monjid@kijamii.com>
2023-02-11 19:23:29 +0200

    add migration files

---
+++ database/migrations/2023_02_11_170919_create_booking_station_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('booking_station', function (Blueprint $table) {
+            $table->id();
+            $table->integer('booking_id');
+            $table->integer('station_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('booking_station');
+    }
+};
